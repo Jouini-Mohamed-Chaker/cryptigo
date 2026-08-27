bbfc2d697cac81d5953cb4c2f085a5d9309fbaa2
Jouini-Mohamed-Chaker <JouiniMohamedChaker@proton.me>
2026-08-27 13:47:01 +0100

    Encryption method implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Jouini-Mohamed-Chaker/cryptigo
 
 go 1.27.0
+
+require (
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+)
--- internal/crypt/encrypt.go
+++ internal/crypt/encrypt.go
@@ -1,5 +1,84 @@
 package crypt
 
-func Encrypt(inFile, outFile, Password string) error {
-	panic("unimplemented")
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"os"
+
+	"golang.org/x/crypto/argon2"
+)
+
+const (
+	SALT_SIZE_IN_BYTES = 16
+	NONCE_SIZE_IN_BYTES = 12
+)
+
+// Params defines the config for Argon2id (ie the params passed to IDKey function, exluding the password and salt)
+type Params struct {
+	Time uint32
+	Memory uint32
+	Threads uint8
+	KeyLen uint32
+}
+
+func Encrypt(inFile, outFile, password string) error {
+	// functions params are already validated
+
+	salt := make([]byte, SALT_SIZE_IN_BYTES)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, NONCE_SIZE_IN_BYTES)
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return err
+	}
+
+	cfg := Params{
+		Time: 1,
+		Memory: 64 * 1024, // 64 MB
+		Threads: 4,
+		KeyLen: 32,
+	}
+
+	secretKey := argon2.IDKey([]byte(password), salt, cfg.Time, cfg.Memory, cfg.Threads, cfg.KeyLen)
+
+	// Initialize raw AES-256 block engine
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return err
+	}
+
+	// Wrap the block engine in GCM mode so it can handle any file size
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// TODO: replace reading the whole file into memory to chunking  
+	file, err := os.ReadFile(inFile)
+	if err != nil {
+		return err
+	}
+
+	// Append salt and nonce to the file
+	var encryptedFileBytes []byte
+	encryptedFileBytes = append(encryptedFileBytes, salt...)
+	encryptedFileBytes = append(encryptedFileBytes, nonce...)
+	
+	// gcm.Seal() scrambles the file and attaches the integrity check tag to the end
+	// First param is the destination slice 
+	encryptedFileBytes = gcm.Seal(encryptedFileBytes, nonce, file, nil)
+
+	// encrypted file format: [ 16-byte Salt ] [ 12-byte Nonce ] [ Encrypted Data + Tag ]
+	// Save to disk
+	err = os.WriteFile(outFile, encryptedFileBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
